2d0f23c7b679923007676b63adb2599ae29b4f88
nezzar <kefifnezzar@gmai.com>
2018-05-31 12:36:18 +0200

    solution 1

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,11 @@
 let nextUserId = 0
-export const addUser = () => ({
+export const addUser = (userName) => ({
+    type: 'ADD_USER',
+    name: userName,
+    id: nextUserId++
 })
 
-export const deleteUser = () => ({
+export const deleteUser = (userId) => ({
+    type: 'DELETE_USER', 
+    id: userId
 })
--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,6 +1,15 @@
 const users = (state = [], action) => {
   switch (action.type) {
-    default:
+    case 'ADD_USER':
+      return  state.concat({
+        name: action.name,
+        id: action.id
+      })
+    case 'DELETE_USER':
+      return state.filter((user) => {
+        return (user.id === action.id ? false : true)
+      })
+    default: 
       return state
   }
 }
