a92be0e7d9a000b8c9fbc9dcc452e996b3e23176
Lucas Vancompernolle <lucas.vancompernolle@epitech.eu>
2018-05-31 10:49:10 +0200

    removed action and reducers

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,6 @@
 let nextUserId = 0
-export const addUser = (name) => ({
-  type: 'ADD_USER',
-  id: nextUserId++,
-  name: name
+export const addUser = () => ({
 })
 
-export const deleteUser = (id) => ({
-  type: 'DEL_USER',
-  id: id
+export const deleteUser = () => ({
 })
