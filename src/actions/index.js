2f9c3d607a2c12556d38949e685785845fef0b79
Maxence Delaine <maxence.delaine@gmail.com>
2018-06-03 12:25:32 +0200

    Adding the files index.js and users.js modified during the first part of the Redux dojo

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,12 @@
 let nextUserId = 0
-export const addUser = () => ({
+
+export const addUser = (user) => ({
+  type: 'ADD_USER',
+  name: user,
+  id: nextUserId++
 })
 
-export const deleteUser = () => ({
+export const deleteUser = (id) => ({
+  type: 'REMOVE_USER',
+  id: id
 })
--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,5 +1,12 @@
 const users = (state = [], action) => {
   switch (action.type) {
+    case 'ADD_USER':
+      return [ ...state, {
+        name: action.name,
+        id: action.id
+      }]
+    case 'REMOVE_USER':
+      return state.filter(user => user.id !== action.id)
     default:
       return state
   }
