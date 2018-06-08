81a15cd81ba1dbe3c77b4e6cddfdf897e09b8074
nezzar <kefifnezzar@gmai.com>
2018-06-08 12:01:08 +0200

    dojo: completed (fuck yeah)

--- src/containers/AddUser.js
+++ src/containers/AddUser.js
@@ -1,20 +1,37 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux'
+import { addUser } from '../actions/index'
+
 
 class AddUser extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      name: ''
     }
+  }
+
+  getCredentials(e){
+    this.setState({
+      name: e.target.value
+    })
+  }
+
+  handleClick(){
+    this.props.dispatch(addUser(this.state.name))
   }
 
   render() {
     return (
         <div>
+          <input type='text' placeholder="Nom" onChange={this.getCredentials.bind(this)} value={this.state.name} />
+          <button type='submit' onClick={this.handleClick.bind(this)}></button>
         </div>
     );
   }
+
+
 }
 
 export default connect()(AddUser)
--- src/containers/UsersList.js
+++ src/containers/UsersList.js
@@ -1,10 +1,13 @@
 import { connect } from 'react-redux'
 import UsersList from '../components/UsersList'
+import { deleteUser } from '../actions/index';
 
 const mapStateToProps = state => ({
+    users: state.users 
 })
 
 const mapDispatchToProps = dispatch => ({
+    deleteUser: (id) => dispatch(deleteUser(id)) 
 })
 
 export default connect(
